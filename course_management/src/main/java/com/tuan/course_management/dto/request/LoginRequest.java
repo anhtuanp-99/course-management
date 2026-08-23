472326607d91620e5d5d57bb06f9be695343fb8e
anhtuanp-99 <anhtuanhl2004@gmail.com>
2026-08-24 00:13:14 +0700

    update entity

---
+++ course_management/src/main/java/com/tuan/course_management/dto/request/LoginRequest.java
@@ -0,0 +1,4 @@
+package com.tuan.course_management.dto.request;
+
+public class LoginRequest {
+}
--- course_management/src/main/java/com/tuan/course_management/entity/Reviews.java
+++ course_management/src/main/java/com/tuan/course_management/entity/Reviews.java
@@ -1,8 +1,11 @@
 package com.tuan.course_management.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "reviews")
@@ -11,6 +14,32 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@ToString(exclude = )
+@ToString(exclude = {"course","student"})
 public class Reviews {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id", nullable = false)
+    private Courses course;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id", nullable = false)
+    private Users student;
+
+    @Column(nullable = false)
+    private Integer rating; // 1-5
+
+    @Column(columnDefinition = "TEXT")
+    private String comment;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
