c222660b4e5a1f747a76149e9611c52c09051e18
anhtuanp-99 <anhtuanhl2004@gmail.com>
2026-08-31 01:16:56 +0700

    refactor: CourseDetailResponse

--- course_management/src/main/java/com/tuan/course_management/dto/response/CourseDetailResponse.java
+++ course_management/src/main/java/com/tuan/course_management/dto/response/CourseDetailResponse.java
@@ -1,6 +1,5 @@
 package com.tuan.course_management.dto.response;
 
-import com.tuan.course_management.entity.Lesson;
 import com.tuan.course_management.enums.CourseStatus;
 import lombok.*;
 
@@ -20,11 +19,11 @@ public class CourseDetailResponse {
     private Long id;
     private String title;
     private String description;
-    private Long teacher_id;    // ID của giảng viên phụ trách
+    private Long teacherId;    // ID của giảng viên phụ trách
     private String teacherName; // Tên giảng viên (đã map)
     private CourseStatus status;
-    private LocalDateTime createAt;
-    private LocalDateTime updateAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
     private List<LessonResponse> lessons;
 
 }
