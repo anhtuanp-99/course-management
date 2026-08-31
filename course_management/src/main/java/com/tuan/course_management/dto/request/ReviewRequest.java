f708a9a542ad1b89479e3294e9629e92c97442ae
anhtuanp-99 <anhtuanhl2004@gmail.com>
2026-08-31 17:36:24 +0700

    feat: ReviewRequest

--- course_management/src/main/java/com/tuan/course_management/dto/request/ReviewRequest.java
+++ course_management/src/main/java/com/tuan/course_management/dto/request/ReviewRequest.java
@@ -1,4 +1,5 @@
 package com.tuan.course_management.dto.request;
+
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
