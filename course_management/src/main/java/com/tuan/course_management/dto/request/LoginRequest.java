e1cd8f062cbc476c2e44fdbbb1cd61be979f5f36
anhtuanp-99 <anhtuanhl2004@gmail.com>
2026-09-01 18:15:36 +0700

    refactor: LoginRequest

--- course_management/src/main/java/com/tuan/course_management/dto/request/LoginRequest.java
+++ course_management/src/main/java/com/tuan/course_management/dto/request/LoginRequest.java
@@ -1,12 +1,8 @@
 package com.tuan.course_management.dto.request;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
-/**
- * DTO nhận dữ liệu từ request đăng nhập.
- */
 @Getter
 @Setter
 @NoArgsConstructor
@@ -14,9 +10,8 @@ import lombok.*;
 @Builder
 public class LoginRequest {
 
-    @NotBlank(message = "Email không được để trống")
-    @Email(message = "Email không đúng định dạng")
-    private String email;
+    @NotBlank(message = "Tên tài khoản không được để trống")
+    private String username;
 
     @NotBlank(message = "Mật khẩu không được để trống")
     private String password;
