5ad27ce87de2cc9b0dd272fa17dd5bdb79ec44ff
anhtuanp-99 <anhtuanhl2004@gmail.com>
2026-09-01 18:13:19 +0700

    refactor: ChangePasswordRequest

--- course_management/src/main/java/com/tuan/course_management/dto/request/ChangePasswordRequest.java
+++ course_management/src/main/java/com/tuan/course_management/dto/request/ChangePasswordRequest.java
@@ -4,9 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
-/**
- * DTO dùng để thay đổi mật khẩu người dùng.
- */
 @Getter
 @Setter
 @NoArgsConstructor
@@ -18,6 +15,6 @@ public class ChangePasswordRequest {
     private String oldPassword;
 
     @NotBlank(message = "Mật khẩu mới không được để trống")
-    @Size(min = 6, message = "Mật khẩu tối thiểu 6 kí tự")
+    @Size(min = 6, message = "Mật khẩu mới phải có tối thiểu 6 ký tự")
     private String newPassword;
 }
